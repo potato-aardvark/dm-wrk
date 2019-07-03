4e9ecc39ce7e79f3bde8e55498e64bdc180eba3a
haoxu <haoxu@cita.utoronto.ca>
2019-07-03 09:43:36 -0400

    Improve eat somewhat

--- correctdms/split-dms/svd-split-eat.py
+++ correctdms/split-dms/svd-split-eat.py
@@ -52,7 +52,9 @@ data = plm.remove_rfi(data, data[-100:], fill='nan')
 # subtract off-pulse stuff
 offpulse = np.nanmean(data[:100])
 data -= offpulse
-svd_data -= offpulse
+svd_data[np.logical_not(np.isnan(data))] -= offpulse
+# (we don't want to subtract from removed RFI bands)
+
 # bin
 binneddata = plm.bin_data(
         data, args.time_bin_sz, args.freq_bin_sz
@@ -78,85 +80,122 @@ svd_modeone = (bdat_u * bdat_s_m1 @ bdat_vh).T
 svd_resid = binneddata - svd_modeone
 
 if plot_save_loc:
-    # pulse as waterfall
-    plt.imshow(binneddata.transpose(),
-            aspect='auto',
-            extent=[0, args.time_total, args.minfreq, args.maxfreq]
+    plt.rcParams['figure.figsize'] = (24, 8)
+    # combined data/model/residual plot
+    fig, (ax0, ax1, ax2) = plt.subplots(
+            1,
+            3,
+            sharex='all',
+            sharey='all',
+            constrained_layout=True
     )
+    cb_min = np.nanmin([svd_resid, binneddata, svd_modeone])
+    cb_max = np.nanmax([svd_resid, binneddata, svd_modeone])
+    print(cb_min, cb_max)
+
+    im = ax0.imshow(
+            binneddata.T,
+            extent=[0, args.time_total, args.minfreq, args.maxfreq],
+            vmin=cb_min, vmax=cb_max, aspect='auto'
+    )
+    ax1.imshow(svd_modeone.T,
+            extent=[0, args.time_total, args.minfreq, args.maxfreq],
+            vmin=cb_min, vmax=cb_max, aspect='auto'
+    )
+    ax2.imshow(svd_resid.T,
+            extent=[0, args.time_total, args.minfreq, args.maxfreq],
+            vmin=cb_min, vmax=cb_max, aspect='auto'
+    )
+    # plt.tight_layout()
     plt.suptitle(args.suptitle)
-    plt.xlabel('time (ms)')
-    plt.ylabel('frequency (MHz)')
+    ax0.set_title('data')
+    ax1.set_title('model')
+    ax2.set_title('residual')
+    fig.colorbar(im, ax=(ax0, ax1, ax2), shrink=0.6)
+
     plt.savefig(
-            os.path.join(plot_save_loc, args.runname) + 'pulse.png'
+            os.path.join(plot_save_loc, args.runname) + '_full.png'
     )
-    
-    # residuals as waterfall
-    plt.imshow(svd_resid.transpose(),
-            aspect='auto',
-            extent=[0, args.time_total, args.minfreq, args.maxfreq]
-    )
-    plt.suptitle(args.suptitle)
-    plt.xlabel('time (ms)')
-    plt.ylabel('frequency (MHz)')
-    plt.savefig(os.path.join(plot_save_loc, args.runname) + '_resids.png')
-    
-    # svd mode 1 as waterfall
-    plt.imshow(svd_modeone.transpose(),
-            aspect='auto',
-            extent=[0, args.time_total, args.minfreq, args.maxfreq]
-    )
-    plt.suptitle(args.suptitle)
-    plt.xlabel('time (ms)')
-    plt.ylabel('frequency (MHz)')
-    plt.savefig(os.path.join(plot_save_loc, args.runname) + '_svdmd1.png')
-    
-    plt.rcParams['figure.figsize'] = (12, 80)
-    
-    # pulse as lines
-    resid_scale = -2
-    plt.figure()
-    for freq_index in range(binneddata.shape[1]):
-        plt.plot(
-                np.linspace(0, args.time_total, binneddata.shape[0]),
-                binneddata[:, freq_index] + resid_scale * freq_index
-        )
-        if not np.any(np.isnan(binneddata[:, freq_index])):
-            plt.axhline(resid_scale * freq_index, color='black', lw=0.5)
-    plt.xlabel('time (ms)')
-    plt.ylabel('frequency')
-    plt.savefig(
-            os.path.join(plot_save_loc, args.runname) + '_pulseprf.png'
-    )
-    
-    # residual as lines
-    plt.figure()
-    for freq_index in range(svd_resid.shape[1]):
-        plt.plot(
-                np.linspace(0, args.time_total, binneddata.shape[0]),
-                svd_resid[:, freq_index] + resid_scale * freq_index
-        )
-        if not np.any(np.isnan(svd_resid[:, freq_index])):
-            plt.axhline(resid_scale * freq_index, color='black', lw=0.5)
-    plt.xlabel('time (ms)')
-    plt.ylabel('frequency')
-    plt.savefig(
-            os.path.join(plot_save_loc, args.runname) + '_residsprf.png'
-    )
-    
-    # mode one as lines
-    plt.figure()
-    for freq_index in range(svd_modeone.shape[1]):
-        plt.plot(
-                np.linspace(0, args.time_total, binneddata.shape[0]),
-                svd_modeone[:, freq_index] + resid_scale * freq_index
-        )
-        if not np.any(np.isnan(svd_modeone[:, freq_index])):
-            plt.axhline(resid_scale * freq_index, color='black', lw=0.5)
-    plt.xlabel('time (ms)')
-    plt.ylabel('frequency')
-    plt.savefig(
-            os.path.join(plot_save_loc, args.runname) + '_modeoneprf.png'
-    )
+
+#     # pulse as waterfall
+#     plt.imshow(binneddata.transpose(),
+#             aspect='auto',
+#             extent=[0, args.time_total, args.minfreq, args.maxfreq]
+#     )
+#     plt.suptitle(args.suptitle)
+#     plt.xlabel('time (ms)')
+#     plt.ylabel('frequency (MHz)')
+#     plt.savefig(
+#             os.path.join(plot_save_loc, args.runname) + 'pulse.png'
+#     )
+#     
+#     # residuals as waterfall
+#     plt.imshow(svd_resid.transpose(),
+#             aspect='auto',
+#             extent=[0, args.time_total, args.minfreq, args.maxfreq]
+#     )
+#     plt.suptitle(args.suptitle)
+#     plt.xlabel('time (ms)')
+#     plt.ylabel('frequency (MHz)')
+#     plt.savefig(os.path.join(plot_save_loc, args.runname) + '_resids.png')
+#     
+#     # svd mode 1 as waterfall
+#     plt.imshow(svd_modeone.transpose(),
+#             aspect='auto',
+#             extent=[0, args.time_total, args.minfreq, args.maxfreq]
+#     )
+#     plt.suptitle(args.suptitle)
+#     plt.xlabel('time (ms)')
+#     plt.ylabel('frequency (MHz)')
+#     plt.savefig(os.path.join(plot_save_loc, args.runname) + '_svdmd1.png')
+#     
+#     plt.rcParams['figure.figsize'] = (12, 80)
+#     
+#     # pulse as lines
+#     resid_scale = -2
+#     plt.figure()
+#     for freq_index in range(binneddata.shape[1]):
+#         plt.plot(
+#                 np.linspace(0, args.time_total, binneddata.shape[0]),
+#                 binneddata[:, freq_index] + resid_scale * freq_index
+#         )
+#         if not np.any(np.isnan(binneddata[:, freq_index])):
+#             plt.axhline(resid_scale * freq_index, color='black', lw=0.5)
+#     plt.xlabel('time (ms)')
+#     plt.ylabel('frequency')
+#     plt.savefig(
+#             os.path.join(plot_save_loc, args.runname) + '_pulseprf.png'
+#     )
+#     
+#     # residual as lines
+#     plt.figure()
+#     for freq_index in range(svd_resid.shape[1]):
+#         plt.plot(
+#                 np.linspace(0, args.time_total, binneddata.shape[0]),
+#                 svd_resid[:, freq_index] + resid_scale * freq_index
+#         )
+#         if not np.any(np.isnan(svd_resid[:, freq_index])):
+#             plt.axhline(resid_scale * freq_index, color='black', lw=0.5)
+#     plt.xlabel('time (ms)')
+#     plt.ylabel('frequency')
+#     plt.savefig(
+#             os.path.join(plot_save_loc, args.runname) + '_residsprf.png'
+#     )
+#     
+#     # mode one as lines
+#     plt.figure()
+#     for freq_index in range(svd_modeone.shape[1]):
+#         plt.plot(
+#                 np.linspace(0, args.time_total, binneddata.shape[0]),
+#                 svd_modeone[:, freq_index] + resid_scale * freq_index
+#         )
+#         if not np.any(np.isnan(svd_modeone[:, freq_index])):
+#             plt.axhline(resid_scale * freq_index, color='black', lw=0.5)
+#     plt.xlabel('time (ms)')
+#     plt.ylabel('frequency')
+#     plt.savefig(
+#             os.path.join(plot_save_loc, args.runname) + '_modeoneprf.png'
+#     )
 
 if data_save_loc:
     np.savez(os.path.join(data_save_loc, args.runname),
