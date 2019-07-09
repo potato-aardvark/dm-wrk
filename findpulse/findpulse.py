9b14577dc062fbf48ad91ea0de380d2685e15f1b
haoxu <haoxu@cita.utoronto.ca>
2019-07-09 15:57:13 -0400

    Stop findpulse overwrite data file (makes it easier to continue stopped operation

--- findpulse/findpulse.py
+++ findpulse/findpulse.py
@@ -2,6 +2,7 @@ import argparse
 
 import numpy as np
 import astropy.units as u
+from astropy import time
 import pipeline as plm
 from baseband import vdif
 
@@ -12,23 +13,26 @@ parser.add_argument(
         help='the time delta in ms'
 )
 parser.add_argument('runname', default='myrun', help='the name of the run')
-parser.add_argument('index', help='the number of timedeltas to read')
+parser.add_argument('start', type=int, help='the start index')
+parser.add_argument('end', type=int, help='the end index')
 parser.add_argument('--data', nargs='+', default=[], help='the baseband data files')
 parser.add_argument('--out', nargs='?', help='where to write the data')
 parser.add_argument('--dm', type=float, default=26.67, help='dm')
 args = parser.parse_args()
 
 fh = vdif.open(args.data)
-plm = plm.WaterfallIntensityPipeline(fh,
+pl = plm.WaterfallIntensityPipeline(fh,
         args.dm,
         800*u.MHz,
         2**20,    ###
-        np.linspace(800, 400, 1024)*u.Mz,
+        np.linspace(800, 400, 1024)*u.MHz,
         1
 )
-for i in range(index):
+
+
+for i in range(int(args.start), int(args.end)):
     data = pl.read_time(args.timedelta)
     data = plm.remove_rfi(data)
     data = np.nansum(data)
-    with open(args.out, 'w') as f:
-        f.write('{} {}'.format(args.index, data))
+    with open(args.out, 'a') as f:
+        f.write('{} {}\n'.format(i, data))
