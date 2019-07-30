dcc2001b305cad24dd5eb5e0457e9dec1e67a949
Xinghao Xu <haoxu@tpb4.sunnyvale>
2019-07-30 12:25:38 -0400

    findpulse change

--- findpulse/dofindpulse.sh
+++ findpulse/dofindpulse.sh
@@ -1,3 +1,14 @@
-#!/bin/bash
+#!/bin/bash -l
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=48:00:00
+#PBS -r n
+#PBS -j oe
+#PBS -q workq
 
-python findpulse.py 5 testrun $1 $2 --data /mnt/scratch-lustre/haoxu/B0329data/0818/{0000000..0040999}.vdif --out /mnt/scratch-lustre/haoxu/out/testrun/testrun.txt
+module load python/conda3-5.3
+source ~/.virtualenvs/vscint/bin/activate 
+module load use.own
+module load scintpipe
+
+# cd $PBS_O_WORKDIR
+python findpulse.py 5 run0818 $1 $2 $3 --data /mnt/scratch-lustre/haoxu/B0329data/0818/ --datafilestart 0 --datafileend 199999 --out /mnt/scratch-lustre/haoxu/out/testrun/0818find_test.txt
--- findpulse/findpulse.py
+++ findpulse/findpulse.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 import numpy as np
 import astropy.units as u
@@ -16,16 +17,24 @@ parser.add_argument('runname', default='myrun', help='the name of the run')
 parser.add_argument('start', type=int, help='the start index')
 parser.add_argument('end', type=int, help='the end index')
 parser.add_argument('skip', type=int, default=None, help='the skip slice bit')
-parser.add_argument('--data', nargs='+', default=[], help='the baseband data files')
+parser.add_argument('--data', help='the baseband data folder')
+parser.add_argument('--datafilestart', type=int, help='the first data file')
+parser.add_argument('--datafileend',
+        type=int, help='the last data file (inclusive)')
 parser.add_argument('--out', nargs='?', help='where to write the data')
 parser.add_argument('--dm', type=float, default=26.67, help='dm')
 args = parser.parse_args()
 
-fh = vdif.open(args.data)
+fh = vdif.open(
+    [
+        os.path.join(args.data, '{:0>7}.vdif'.format(i))
+        for i in range(args.datafilestart, args.datafileend + 1)
+    ]
+)
 pl = plm.WaterfallIntensityPipeline(fh,
         args.dm,
         800*u.MHz,
-        2**19,    ###
+        2**18,    ###
         np.linspace(800, 400, 1024)*u.MHz,
         1
 )
