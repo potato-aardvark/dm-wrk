cc0edfcc113aaa4f367ee04dc9cb278e813f7a9d
haoxu <haoxu@cita.utoronto.ca>
2019-07-31 10:28:21 -0400

    Last commit before tearing up findpulse

--- findpulse/autoplot.py
+++ findpulse/autoplot.py
@@ -4,9 +4,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.rcParams['figure.figsize'] = (12, 8)
 
-t_ind, intensity = np.loadtxt('/mnt/scratch-lustre/haoxu/out/testrun/testrun.txt', unpack=True)
-times = t_ind * 0.005
-plt.plot(times, intensity)
+tmf, intensity = np.loadtxt('/mnt/scratch-lustre/haoxu/out/testrun/0818find.txt', unpack=True)
+times = tmf * 0.005
+tim_ind = np.argsort(times)
+plt.plot(times[tim_ind], intensity[tim_ind])
 plt.xlabel('time (s)')
 plt.ylabel('intensity (arb. units)')
 plt.show()
