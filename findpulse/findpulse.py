d5b231b8cf434ea4bfade24d49497eeba2f92a5f
haoxu <haoxu@cita.utoronto.ca>
2019-08-14 11:51:59 -0400

    Polyco + folding for findpulse

--- findpulse/findpulse.py
+++ findpulse/findpulse.py
@@ -8,21 +8,22 @@ from baseband import vdif
 from scintillometry.integration import Stack
 from scintillometry.dispersion import Dedisperse
 from scintillometry.functions import Square
+from scintillometry.phases import PolycoPhase
 
 parser = argparse.ArgumentParser()
 parser.add_argument('runname', default='myrun', help='the name of the run')
 parser.add_argument('start', type=int, help='the start index')
 parser.add_argument('end', type=int, help='the end index')
 parser.add_argument('skip', type=int, default=None, help='the skip slice bit')
+parser.add_argument('polyco_file', help='where the polyco.dat file is')
 parser.add_argument('--data', help='the baseband data folder')
 parser.add_argument('--datafilestart', type=int, help='the first data file')
 parser.add_argument('--datafileend',
         type=int, help='the last data file (inclusive)')
-parser.add_argument('--out', nargs='?', help='where to write the data')
+parser.add_argument('out', help='where to write the data')
 parser.add_argument('--dm', type=float, default=26.67, help='dm')
 args = parser.parse_args()
 
-period = 0.714519699726 * u.s
 
 fh = vdif.open(
     [
@@ -31,18 +32,17 @@ fh = vdif.open(
     ]
 )
 
-pl = plm.WaterfallIntensityPipeline(fh,
-        args.dm,
-        800*u.MHz,
-        2**18,    ###
-        np.linspace(800, 400, 1024)*u.MHz,
-        1
-)
+dedisp = Dedisperse(fh, 26.67, reference_frequency=540*u.MHz,
+        frequency=np.linspace(800., 400., 1024)*u.MHz, sideband=1)
 
+sq_dedisp = Square(dedisp)
+
+stack = Stack(sq_dedisp, 2**18, PolycoPhase(args.polyco_file))
+ 
 for i in range(int(args.start), int(args.end), int(args.skip)):
-    pl.outstream.seek(args.timedelta * i)
-    data = pl.read_time(args.timedelta)
-    data = plm.remove_rfi(data)
-    data = np.nansum(data)
-    with open(args.out, 'a') as f:
-        f.write('{} {}\n'.format(i, data))
+    stack.seek(i)
+    rawdata = stack.read(1)
+    data = rawdata[0, 110000:135000]
+    np.savez_compressed(os.path.join(args.out, str(i)), data=data,
+            i=np.array(i))
+
