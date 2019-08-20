61da38c25abbd7536859c8e1dc06d6140e58b81d
haoxu <haoxu@cita.utoronto.ca>
2019-08-20 13:43:10 -0400

    I did some c h a n g e

--- findpulse/findpulse.py
+++ findpulse/findpulse.py
@@ -13,17 +13,27 @@ from scintillometry.phases import PolycoPhase
 parser = argparse.ArgumentParser()
 parser.add_argument('runname', default='myrun', help='the name of the run')
 parser.add_argument('start', type=int, help='the start index')
-parser.add_argument('end', type=int, help='the end index')
+parser.add_argument('num', type=int, help='the number of pulses')
 parser.add_argument('skip', type=int, default=None, help='the skip slice bit')
 parser.add_argument('polyco_file', help='where the polyco.dat file is')
 parser.add_argument('--data', help='the baseband data folder')
 parser.add_argument('--datafilestart', type=int, help='the first data file')
 parser.add_argument('--datafileend',
         type=int, help='the last data file (inclusive)')
+parser.add_argument('pulse_start', type=int,
+        help='the phase index where the pulse starts')
+parser.add_argument('pulse_end', type=int, 
+        help='the phase index where the pulse ends')
 parser.add_argument('out', help='where to write the data')
 parser.add_argument('--dm', type=float, default=26.67, help='dm')
 args = parser.parse_args()
 
+orig_n_phases = 2**18
+n_phases = 2**10
+# Since the arguments passed in expect n_phases = 2**18, and we have 
+# n_phases = 2**10, we need to adjust downwards.
+args.pulse_start = (args.pulse_start * n_phases) // orig_n_phases
+args.pulse_end = (args.pulse_end * n_phases) // orig_n_phases
 
 fh = vdif.open(
     [
@@ -33,16 +43,17 @@ fh = vdif.open(
 )
 
 dedisp = Dedisperse(fh, 26.67, reference_frequency=540*u.MHz,
-        frequency=np.linspace(800., 400., 1024)*u.MHz, sideband=1)
+        samples_per_frame=2**18, frequency=np.linspace(800., 400., 1024)*u.MHz,
+        sideband=1)
 
 sq_dedisp = Square(dedisp)
 
-stack = Stack(sq_dedisp, 2**18, PolycoPhase(args.polyco_file))
+# 2 ** 18
+stack = Stack(sq_dedisp, n_phases, PolycoPhase(args.polyco_file))
  
-for i in range(int(args.start), int(args.end), int(args.skip)):
+for i in range(int(args.start), int(args.start + args.num), int(args.skip)):
     stack.seek(i)
-    rawdata = stack.read(1)
-    data = rawdata[0, 110000:135000]
-    np.savez_compressed(os.path.join(args.out, str(i)), data=data,
-            i=np.array(i))
+    data = stack.read(1).sum()
+    with open(os.path.join(args.out, args.runname), 'a') as outfh:
+        outfh.write('{} {}\n'.format(i, data))
 
